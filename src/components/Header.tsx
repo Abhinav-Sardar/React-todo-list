e447c6a4ebbe067e517231f08be17fe2b96de9b3
Abhinav-Sardar <abhinavsardar.podar@gmail.com>
2021-06-30 16:20:47 +0530

    Basic Header finished

---
+++ src/components/Header.tsx
@@ -0,0 +1,17 @@
+import React, { FC, Fragment } from "react";
+//@ts-ignore"
+import Logo from "../Logo.svg";
+//@ts-ignore"
+import styles from "./css/Header.module.css";
+const Header: FC = () => {
+  return (
+    <Fragment>
+      <div className={styles.header__wrapper}>
+        <h3 className={styles.header}>Todo List</h3>
+        <h2>A place to keep track of your work</h2>
+        <i className="fab fa-react"></i>
+      </div>
+    </Fragment>
+  );
+};
+export default Header;
